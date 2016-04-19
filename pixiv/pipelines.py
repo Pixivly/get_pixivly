577f53de085ca608a51932a891f76b3899f3a62a
zhangjie <luoyetx@gmail.com>
2016-04-19 12:50:21 +0800

    remove header

--- pixiv/pipelines.py
+++ pixiv/pipelines.py
@@ -8,11 +8,6 @@ from scrapy.pipelines.images import ImagesPipeline, DropItem
 class PixivPipeline(ImagesPipeline):
 
     headers = {
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-        'Accept-Encoding': 'gzip, deflate, sdch',
-        'Host': '',
-        'Referer': '',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.65 Safari/537.36',
     }
 
     def file_path(self, request, response=None, info=None):
